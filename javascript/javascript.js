35774d0ab97bd49a54b52f3c26102d2f7f8e660c
Mirabed <irianharyllbecera@gmail.com>
2025-03-30 12:38:25 +0800

    done

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,111 +1,38 @@
-function getComputerChoice() {
-    return Math.floor(Math.random() * 3) + 1;
-}
+let container = document.querySelector("#container");
+let square = document.querySelector("#square");
 
-function getHumanChoice() {
-    return prompt("Input your choice: ");
-}
+createGrid(16);
 
-function playRound(humanChoice, computerChoice) {
+square.addEventListener("click",()=>{
+    let num = parseInt(prompt("Number of Squares: "));
+    container.textContent = '';
 
-    let rounds = parseInt(prompt("How many rounds would you want to play: "));
-    while (rounds > 0) {
+    createGrid(num);
 
-        humanChoice = getHumanChoice();
-        humanChoice = humanChoice.toUpperCase();
-        computerChoice = getComputerChoice();
-        let i = 0;
+});
 
+function createGrid(num){
+    let opa = 0.1;
 
-        switch (humanChoice) {
-            case "ROCK":
-                i = 1;
-                if (i === computerChoice) {
-                    console.log("It's a Tie.");
-                } else if (i < computerChoice && computerChoice !== 2) {
-                    console.log("You Win.");
-                    humanScore++;
-                } else {
-                    console.log("You Lose.");
-                    computerScore++;
-                }
-                break;
-            case "PAPER":
-                i = 2;
-                if (i === computerChoice) {
-                    console.log("It's a Tie.");
-                } else if (i > computerChoice && computerChoice !== 3) {
-                    console.log("You Win.");
-                    humanScore++;
-                } else {
-                    console.log("You Lose.");
-                    computerScore++;
-                }
-                break;
-            case "SCISSOR":
-                i = 3;
-                if (i === computerChoice) {
-                    console.log("It's a Tie.");
-                } else if (i > computerChoice && computerChoice !== 1) {
-                    console.log("You Win.");
-                    humanScore++;
-                } else {
-                    console.log("You Lose.");
-                    computerScore++;
-                }
-                break;
-        }
+    for(let i=0;i<num*num;i++){
+        let grid = document.createElement('div');
+        grid.classList.add('grid-item');
+        grid.style.flexBasis = `calc(100% / ${num})`;
+        container.appendChild(grid);
 
-        humanDecision(i);
-        computerDecision(computerChoice);
-        console.log(`Human Score: ${humanScore}`);
-        console.log(`Computer Score: ${computerScore}`);
-        console.log("");
-
-        rounds--;
-    };
-
-    if (humanScore > computerScore)
-        return "All in all the Humans win for now.";
-    else if (humanScore < computerScore)
-        return "The computer wins, AI will take over.";
-    else {
-        return "Computer and Humans are equal.";
-    }
-
-}
-
-function humanDecision(decision) {
-    switch (decision) {
-        case 1:
-            console.log("You choose Rock.");
-            break;
-        case 2:
-            console.log("You choose Paper.");
-            break;
-        case 3:
-            console.log("You choose Scissor.");
-            break;
+        grid.addEventListener("mouseenter",()=>{
+            grid.style.backgroundColor = randomColors();
+        });
+        grid.addEventListener("mouseleave",()=>{
+            grid.style.backgroundColor = "black";
+            grid.style.opacity = opa+=0.1;
+        });
     }
 }
 
-function computerDecision(decision) {
-    switch (decision) {
-        case 1:
-            console.log("The computer choses Rock.");
-            break;
-        case 2:
-            console.log("The computer choses Paper.");
-            break;
-        case 3:
-            console.log("The computer choses Scissor.");
-            break;
-    }
+function randomColors(){
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    return `rgb(${red},${green},${blue})`;
 }
-
-let humanScore = 0;
-let computerScore = 0;
-
-console.log(playRound());
-
-
